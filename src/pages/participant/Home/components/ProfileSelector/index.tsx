b534f3352b5b72920265ab5fb493f04c77281c4e
KimYuKyung <kyg0954@naver.com>
2025-04-09 21:23:25 +0900

    feat: 참여자 홈의 유저 입력 페이지 분리
    - 참여자 홈의 유저 입력 페이지 분리
    - ProfileSelector 컴포넌트 분리 & 메모이제이션
    - customAlert 수정

---
+++ src/pages/participant/Home/components/ProfileSelector/index.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+function ProfileSelector({
+  profileRef,
+  visible,
+  setVisible,
+  setSelectedProfile,
+}: {
+  profileRef: React.RefObject<HTMLImageElement>;
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setSelectedProfile: React.Dispatch<React.SetStateAction<number>>;
+}) {
+  const profileSelectorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (!visible) return;
+      if (
+        !profileSelectorRef.current?.contains(e.target as Node) &&
+        !profileRef.current?.contains(e.target as Node)
+      ) {
+        setVisible(false);
+      }
+    };
+    document.addEventListener('click', handleOutsideClick, true);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick, true);
+    };
+  }, [profileSelectorRef, profileRef, visible]);
+
+  return (
+    <ProfileSelectorContainer ref={profileSelectorRef} $visible={visible}>
+      <SlidingDoor>
+        <span></span>
+      </SlidingDoor>
+      <Profiles>
+        <img
+          src="/images/defaultProfile-gray1.png"
+          onClick={() => {
+            setSelectedProfile(0);
+          }}
+        />
+        <img
+          src="/images/defaultProfile-gray2.png"
+          onClick={() => {
+            setSelectedProfile(1);
+          }}
+        />
+        <img
+          src="/images/defaultProfile-gray3.png"
+          onClick={() => {
+            setSelectedProfile(2);
+          }}
+        />
+      </Profiles>
+    </ProfileSelectorContainer>
+  );
+}
+
+export default React.memo(ProfileSelector);
+
+const ProfileSelectorContainer = styled.div<{ $visible: boolean }>`
+  width: 100%;
+  height: 24rem;
+  background-color: #ffffff;
+  position: absolute;
+  bottom: 0;
+  border-radius: 16px 16px 0 0;
+  transform: ${(props) =>
+    props.$visible ? 'translateY(-0%)' : 'translateY(100%)'};
+  transition: ${(props) =>
+    props.$visible ? 'transform 0.6s ease-out' : 'transform 0.6s ease-in'};
+`;
+
+const Profiles = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 3.2rem 1.6rem 3.5rem 1.6rem;
+  gap: 1rem;
+  overflow-x: auto;
+
+  & > img {
+    width: 10rem;
+    height: 10rem;
+    border-radius: 100%;
+    cursor: pointer;
+  }
+`;
+
+const SlidingDoor = styled.div`
+  height: 72.5px;
+  border-bottom: 0.5px solid rgba(60, 60, 67, 0.18);
+  position: relative;
+  display: flex;
+  justify-content: center;
+
+  & > span {
+    display: inline-block;
+    width: 13.4rem;
+    height: 5px;
+    border-radius: 100px;
+    background: rgba(0, 0, 0, 0.4);
+    position: absolute;
+    top: 1.2rem;
+  }
+`;
--- src/utils/customAlert.ts
+++ src/utils/customAlert.ts
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2';
+import Swal, { SweetAlertIcon, SweetAlertPosition } from 'sweetalert2';
 
 export const networkErrorAlert = (
   content = '네트워크 에러<br/>다시 시도해주세요'
@@ -15,13 +15,88 @@ export const networkErrorAlert = (
   });
 };
 
-export const defaultAlert = (content: string) => {
+export const defaultAlert = (title: string, text?: string) => {
   Swal.fire({
-    title: `${content}`,
+    title: `${title}`,
+    ...(text && { text }),
     confirmButtonColor: '#A7C2E4',
     color: '#809dc1',
     background: 'rgba(228, 245, 255, 0.9)',
     width: 400,
     padding: '2rem 0rem 2rem',
+  });
+};
+
+interface AlertProps {
+  position?: SweetAlertPosition;
+  title?: string;
+  text?: string;
+  imageUrl?: string;
+  imageWidth?: number;
+  imageHeight?: number;
+  showConfirmButton?: boolean;
+  timer?: number;
+  width?: number;
+  padding?: string;
+  showClass?: string;
+  hideClass?: string;
+}
+
+export const imageAlert = ({
+  position = 'center',
+  title = `게임이 종료되었습니다.`,
+  text = '게임 결과창으로 이동합니다.',
+  imageUrl = '/images/errorImage.png',
+  imageWidth = 200,
+  imageHeight = 200,
+  showConfirmButton = false,
+  timer = 2300,
+  width = 600,
+  padding = '4em 0rem 4em',
+  showClass = 'animate__animated animate__fadeInDown',
+  hideClass = 'animate__animated animate__fadeOutUp',
+}: Partial<AlertProps> = {}) => {
+  Swal.fire({
+    position,
+    title,
+    text,
+    imageUrl,
+    imageWidth,
+    imageHeight,
+    showConfirmButton,
+    timer,
+    width,
+    padding,
+    showClass: {
+      popup: showClass,
+    },
+    hideClass: {
+      popup: hideClass,
+    },
+  });
+};
+
+export const NoticeAlert = ({
+  title,
+  text,
+  icon,
+  position = 'top',
+}: {
+  title: string;
+  text?: string;
+  icon?: SweetAlertIcon;
+  position?: SweetAlertPosition;
+}) => {
+  Swal.fire({
+    position,
+    title: `${title}`,
+    ...(text && { text }),
+    ...(icon && { icon }),
+    color: '#809dc1',
+    background: 'rgba(228, 245, 255, 0.9)',
+    width: 400,
+    padding: '2rem 0',
+    showConfirmButton: false,
+    timer: 1500,
   });
 };
