114374cf2d46423f327b0c43b9d214f819015e4d
KimYuKyung <kyg0954@naver.com>
2025-04-09 17:45:47 +0900

    feat: socket stomp 방식으로 변경, socket contextApi 적용'
    - nodejs에서 spring 서버로 변경함에 따라 socketio에서 stomp 방식으로 변경
    - socket에 contextAPI 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,45 @@
-import React from 'react';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { registerSocket } from 'socket';
-import Main from '@pages/Main';
-import WaitingRoomLayout from '@pages/WaitingRoom';
-import GameResult from '@pages/GameResult';
-import Buy from '@pages/student/Buy';
-import Sell from '@pages/student/Sell';
-import Wallet from '@pages/student/Wallet';
-import StudentMain from '@pages/student/Main';
+import HostHome from '@pages/host/Home';
+import GameLobby from '@pages/host/GameLobby';
+import GameResult from '@pages/host/GameResult';
+import Buy from '@pages/participant/Buy';
+import Sell from '@pages/participant/Sell';
+import Dashboard from '@pages/participant/Dashboard';
+import ParticipantHome from '@pages/participant/Home';
 import About from '@pages/About';
 import Contact from '@pages/Contact';
-import ErrorPage from '@pages/404Page';
+import Page404 from '@pages/Page404';
+import { StompProvider } from '@contexts/SocketContext';
+import Enter from '@pages/Enter';
+import Home from '@pages/Home';
 
 function App() {
-  const socket = () => {
-    registerSocket();
-  };
-
-  socket();
-
   return (
     <>
       <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/room/wait" element={<WaitingRoomLayout />} />
-          <Route path="/room/result" element={<GameResult />} />
+      <StompProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
 
-          {/* 학생들 */}
-          <Route path="/student" element={<StudentMain />} />
-          <Route path="/student/wallet" element={<Wallet />} />
-          <Route path="/student/buy" element={<Buy />} />
-          <Route path="/student/sell" element={<Sell />} />
+            <Route path="/enter" element={<Enter />} />
+            <Route path="/host" element={<HostHome />} />
+            <Route path="/participant" element={<ParticipantHome />} />
 
-          <Route path={'*'} element={<ErrorPage />} />
-        </Routes>
-      </Router>
+            <Route path="/host/room/wait" element={<GameLobby />} />
+            <Route path="/host/room/result" element={<GameResult />} />
+
+            <Route path="/participant/wallet" element={<Dashboard />} />
+            <Route path="/participant/purchase" element={<Buy />} />
+            <Route path="/participant/sell" element={<Sell />} />
+
+            <Route path={'*'} element={<Page404 />} />
+          </Routes>
+        </Router>
+      </StompProvider>
     </>
   );
 }
