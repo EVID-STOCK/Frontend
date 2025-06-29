0361a8d93681bff5eaa87f4ffec11be521115f2c
KimYuKyung <kyg0954@naver.com>
2025-06-30 04:12:26 +0900

    refactor: fetchStockGraph 에러 처리 제거

--- src/apis/api/stock.ts
+++ src/apis/api/stock.ts
@@ -7,18 +7,10 @@ export const fetchStockGraph = async (
   roomPW: string,
   companyId: number
 ): Promise<ApiResponse<StockGraphPoint[]>> => {
-  try {
-    const response = await defaultInstance.get(`/stocks/${companyId}/graph`, {
-      params: {
-        pwd: roomPW,
-      },
-    });
-    return response.data;
-  } catch (e: any) {
-    if (e.response) {
-      throw new Error(e.response.data.message || '알 수 없는 오류 발생');
-    } else {
-      throw new Error('서버에 연결할 수 없습니다.');
-    }
-  }
+  const response = await defaultInstance.get(`/stocks/${companyId}/graph`, {
+    params: {
+      pwd: roomPW,
+    },
+  });
+  return response.data;
 };
