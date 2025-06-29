05b245594011ad8f814232b99a7574ee0c07a647
KimYuKyung <kyg0954@naver.com>
2025-06-30 04:40:29 +0900

    refactor: axios 전역 에러 처리 추가

--- src/apis/utils/instance.ts
+++ src/apis/utils/instance.ts
@@ -1,4 +1,4 @@
-import { defaultAlert, networkErrorAlert } from '@utils/customAlert';
+import { networkErrorAlert } from '@utils/customAlert';
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BackEndUrl + '/api';
@@ -9,20 +9,20 @@ const defaultApi = axios.create({
   withCredentials: true,
 });
 
-// defaultApi.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (!error.response) {
-//       networkErrorAlert('네트워크에 문제가 있습니다.');
-//     } else if (error.response.status === 500) {
-//       networkErrorAlert(
-//         '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.'
-//       );
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+defaultApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (!error.response) {
+      networkErrorAlert('네트워크에 문제가 있습니다.');
+    } else if (error.response.status === 500) {
+      networkErrorAlert(
+        '서버 오류가 발생했습니다. \n 잠시 후 다시 시도해 주세요.'
+      );
+    }
+    return Promise.reject(error);
+  }
+);
 
 export const defaultInstance = defaultApi;
